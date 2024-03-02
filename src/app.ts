3a74f55134e512e9c15977e7a0f9f79fa96f4eda
Hugo Silva <hugo.marcelo@clevertech.biz>
2024-03-02 18:57:46 -0300

    feat: add use case for register

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,28 @@
-import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
+import { ZodError } from 'zod'
+
+import { env } from './env'
+import { appRoutes } from './http/routes'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
+app.register(appRoutes)
 
-prisma.user.create({
-  data: {
-    name: 'Hugo Marcelo',
-    email: 'hugomarcelo91@gmail.com',
-  },
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    reply.status(400).send({
+      message: 'Validation error.',
+      issues: error.format(),
+    })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: Send error to a monitoring service
+  }
+
+  return reply.status(500).send({
+    message: 'Internal server error.',
+  })
 })
